27242847c0f294cfc3d7c996e1375ca8b4647b2a
platform <whsl4598@gmail.com>
2021-08-30 18:08:48 +0900

    스켈레톤 기반의 끊어짐 없는 인물객체 추적

--- sort.py
+++ sort.py
@@ -32,6 +32,13 @@ from filterpy.kalman import KalmanFilter
 
 np.random.seed(0)
 
+total_ids= []
+Total_Pool = []
+
+occlusion_ids = []
+Occlusion_Pool = []
+evaporation_ids = []
+Evaporation_Pool = []
 
 def linear_assignment(cost_matrix):
   try:
@@ -207,6 +214,119 @@ class Sort(object):
     self.trackers = []
     self.frame_count = 0
 
+  def occlu_iou(self, my_trk, other_trk):
+    """
+    겹쳐지는지 여부 반환
+    """
+    left_top_x_max = np.maximum(my_trk[0][0], other_trk[0][0])
+    left_top_y_max = np.maximum(my_trk[0][1], other_trk[0][1])
+    right_bottom_x_min = np.minimum(my_trk[0][2], other_trk[0][2])
+    right_bottom_y_min = np.minimum(my_trk[0][3], other_trk[0][3])
+    w = np.maximum(0., right_bottom_x_min - left_top_x_max)
+    h = np.maximum(0., right_bottom_y_min - left_top_y_max)
+    wh = w * h
+    over = wh / ((my_trk[0][2] - my_trk[0][0]) * (my_trk[0][3] - my_trk[0][1])
+            + (other_trk[0][2] - other_trk[0][0]) * (other_trk[0][3] - other_trk[0][1]) - wh)
+    return over
+
+  def evap_iou(self, new_trk, over_trk):
+    """
+    겹쳐지는지 여부 반환
+    """
+    left_top_x_max = np.maximum(new_trk[0][0], over_trk['left_top_x'])
+    left_top_y_max = np.maximum(new_trk[0][1], over_trk['left_top_y'])
+    right_bottom_x_min = np.minimum(new_trk[0][2], over_trk['right_bottom_x'])
+    right_bottom_y_min = np.minimum(new_trk[0][3], over_trk['right_bottom_y'])
+    w = np.maximum(0., right_bottom_x_min - left_top_x_max)
+    h = np.maximum(0., right_bottom_y_min - left_top_y_max)
+    wh = w * h
+    over = wh / ((new_trk[0][2] - new_trk[0][0]) * (new_trk[0][3] - new_trk[0][1])
+            + (over_trk['right_bottom_x'] - over_trk['left_top_x']) * (over_trk['right_bottom_y'] - over_trk['left_top_y']) - wh)
+    return over
+
+  def merge_sort(self, array):
+	  if len(array) < 2:
+	  	return array
+	  mid = len(array) // 2
+	  low_arr = self.merge_sort(array[:mid])
+	  high_arr = self.merge_sort(array[mid:])
+
+	  merged_arr = []
+	  l = h = 0
+	  while l < len(low_arr) and h < len(high_arr):
+	  	if low_arr[l]['iou'] > high_arr[h]['iou']:
+	  		merged_arr.append(low_arr[l])
+	  		l += 1
+	  	else:
+	  		merged_arr.append(high_arr[h])
+	  		h += 1
+	  merged_arr += low_arr[l:]
+	  merged_arr += high_arr[h:]
+	  return merged_arr
+
+  def occlusion_overlapping(self, over, trk):
+    """
+    새로운 아이디가 기존 아이디와 같은지 아닌지 반환
+    """
+    global occlusion_ids, Occlusion_Pool
+    # 가장 많이 겹쳐지는 iou의 추적 아이디 할당
+    overlap_max = over
+    idx_max = over
+    for oc_idx, oc_trk in enumerate(Occlusion_Pool): # 새로운 추적 아이디 유지            
+      o_iou = self.occlu_iou(trk, oc_trk)
+      if overlap_max < o_iou:
+        overlap_max = o_iou
+        idx_max = oc_idx
+    if overlap_max > 0:
+      # self.trackers에 있는 추적 아이디도 바꿔줘야함
+      for s_trk in self.trackers:
+        if s_trk.id+1 == trk[0][4]:
+          s_trk.id = int(Occlusion_Pool[idx_max][0][4])-1
+          break
+      # OP에 있는 해당 가려진 인물의 추적 아이디 재할당
+      trk[0][4] = Occlusion_Pool[idx_max][0][4]
+      # OP로부터 재할당한 정보 삭제 및 제거
+      occlusion_ids.remove(trk[0][4])
+      del Occlusion_Pool[idx_max]
+    return overlap_max
+
+  def evaporation_overlapping(self, trk, result):
+    """
+    새로운 아이디가 기존 아이디와 같은지 아닌지 반환
+    """
+    global evaporation_ids, Evaporation_Pool
+    # iou를 계산했을 때 가장 높은 순서대로 재할당
+    # 이미 할당 되어 있는 tracker이면 다음 순서의 tracker 할당
+    # 0만 남아 있으면 바로 0 반환
+    overlaps = []
+    for ev_idx, ev_trk in enumerate(Evaporation_Pool): # 새로운 추적 아이디 유지
+      overlaps.append({'ep_idx': ev_idx, 'trk_id': ev_trk['trk_id'], 'iou': self.evap_iou(trk, ev_trk)})
+    if len(overlaps) == 0: # EP에 아무것도 없다면 0 반환
+      return 0
+    else:
+      overlaps_sorted = self.merge_sort(overlaps)
+      for over_sorted in overlaps_sorted:
+        if over_sorted['iou'] == 0: # 내림차순 정렬했는데도 0이면 바로 0 반환
+          return 0
+        else:
+          already_assigned = False # 이미 할당되어 있다면 그 다음 순서로 할당
+          for res_trk in result:
+            if over_sorted['trk_id'] == res_trk[0][4]:
+              already_assigned = True
+              break
+          if not already_assigned:
+            # self.trackers에 있는 추적 아이디도 바꿔줘야함
+            for s_trk in self.trackers:
+              if s_trk.id+1 == trk[0][4]:
+                s_trk.id = int(over_sorted['trk_id'])-1
+                break
+            # EP에 있는 사라진 인물의 추적 아이디 재할당
+            trk[0][4] = over_sorted['trk_id']
+            # EP로부터 재할당한 정보 삭제 및 제거
+            evaporation_ids.remove(trk[0][4])
+            del Evaporation_Pool[over_sorted['ep_idx']]
+            return over_sorted['iou']
+
   def update(self, dets=np.empty((0, 5))):
     """
     Params:
@@ -248,15 +368,90 @@ class Sort(object):
         # remove dead tracklet
         if(trk.time_since_update > self.max_age):
           self.trackers.pop(i)
+    # if self.frame_count == 5276:
+    #   print(self.frame_count)
     if(len(ret)>0):
-      return np.concatenate(ret)
+      # 0825 가려지거나 사라져서 재식별하는 문제를 해결하려고 OP,EP를 사용해서 재할당하도록 보완
+      global total_ids, Total_Pool, occlusion_ids, Occlusion_Pool, evaporation_ids, Evaporation_Pool
+      result = []
+      success = []
+      failure = []
+      for trk in ret:
+        result.append(trk) # 추적 성공
+        if trk[0][4] in total_ids:
+          success.append(trk[0][4])
+      # ret 크기는 0 이고 total_ids 크기가 아직 있으면 추적 실패한 인물이 있음
+      for id in total_ids:
+        if id not in success:
+          failure.append(id)
+      if len(failure) > 0: # 추적 실패
+        for fail in failure:
+          # OP에 있으면 패쓰
+          if fail not in occlusion_ids:
+            if fail not in evaporation_ids:
+              for trk in Total_Pool:
+                if fail == trk['trk_id']:
+                  evaporation_ids.append(fail)
+                  Evaporation_Pool.append(trk) # OP에 없음, EP에 추가
+
+      for idx, trk in enumerate(result): # 추적 성공, trk[0][4]가 추적 아이디임
+        if trk[0][4] not in total_ids: # 새로운 인물?, 새로운 추적 아이디 유지
+          # OP에 있는 겹친 BBox 현재 영역과 겹쳐진 BBox 과거 영역의 합집합 영역과 겹침?
+          occlu_over = self.occlusion_overlapping(0, trk) # over 초기값은 무조건 0
+          # EP에 있는 사라진 BBox 과거 영역과 겹침?
+          evap_over = self.evaporation_overlapping(trk, result)
+          if (occlu_over == 0) & (evap_over == 0): # 완전히 새로운 인물
+            tracker = {'trk_id' : trk[0][4], 'left_top_x' : trk[0][0], 'left_top_y' : trk[0][1], 'right_bottom_x' : trk[0][2], 'right_bottom_y' : trk[0][3]}
+            total_ids.append(trk[0][4])
+            Total_Pool.append(tracker)
+        else: # 기존 추적 아이디 유지
+          for tracker in Total_Pool:
+            if tracker['trk_id'] == trk[0][4]: # 새로운 정보로 업데이트
+              tracker['left_top_x'] = trk[0][0]
+              tracker['left_top_y'] = trk[0][1]
+              tracker['right_bottom_x'] = trk[0][2]
+              tracker['right_bottom_y'] = trk[0][3]
+        # 다른 BBox와 겹침?
+        over_lap = 0
+        for over_idx, over_trk in enumerate(result):
+          if over_idx != idx:
+            over_lap = self.occlu_iou(trk, over_trk)
+            if over_lap > 0: # 겹쳐진 인물의 추적 아이디를 OP에 삽입
+              if trk[0][4] not in occlusion_ids:
+                occlusion_ids.append(trk[0][4])
+                Occlusion_Pool.append(trk)
+              else:
+                for occ_trk in Occlusion_Pool:
+                  if trk[0][4] == occ_trk[0][4]:
+                    occ_trk[0][0] = trk[0][0]
+                    occ_trk[0][1] = trk[0][1]
+                    occ_trk[0][2] = trk[0][2]
+                    occ_trk[0][3] = trk[0][3]
+        if over_lap == 0:
+        # OP에 이미 삽입됨?
+          if trk[0][4] in occlusion_ids:
+            for idx, oc_trk in enumerate(Occlusion_Pool):
+              if trk[0][4] == oc_trk[0][4]: # OP에서 삭제 및 제거
+                occlusion_ids.remove(trk[0][4])
+                del Occlusion_Pool[idx]
+                break
+        # EP에 이미 삽입됨?
+        if (over_lap > 0) | ((over_lap == 0) & (trk[0][4] in occlusion_ids)):
+          for idx, ev_trk in enumerate(Evaporation_Pool):
+            if trk[0][4] == ev_trk['trk_id']: # EP에서 삭제 및 제거
+              evaporation_ids.remove(trk[0][4])
+              del Evaporation_Pool[idx]
+              break
+        # 다음 프레임으로 이동
+      return np.concatenate(result)
     return np.empty((0,5))
 
 def parse_args():
     """Parse input arguments."""
     parser = argparse.ArgumentParser(description='SORT demo')
     parser.add_argument('--display', dest='display', help='Display online tracker output (slow) [False]',action='store_true')
-    parser.add_argument("--seq_path", help="Path to detections.", type=str, default='data')
+    # parser.add_argument("--seq_path", help="Path to detections.", type=str, default='data')
+    parser.add_argument("--seq_path", help="Path to detections.", type=str, default='my_data')
     parser.add_argument("--phase", help="Subdirectory in seq_path.", type=str, default='train')
     parser.add_argument("--max_age", 
                         help="Maximum number of frames to keep alive a track without associated detections.", 
@@ -297,16 +492,17 @@ if __name__ == '__main__':
     with open(os.path.join('output', '%s.txt'%(seq)),'w') as out_file:
       print("Processing %s."%(seq))
       for frame in range(int(seq_dets[:,0].max())):
-        frame += 1 #detection and frame numbers begin at 1
+        # frame += 1 #detection and frame numbers begin at 1
         dets = seq_dets[seq_dets[:, 0]==frame, 2:7]
         dets[:, 2:4] += dets[:, 0:2] #convert to [x1,y1,w,h] to [x1,y1,x2,y2]
         total_frames += 1
 
         if(display):
-          fn = os.path.join('mot_benchmark', phase, seq, 'img1', '%06d.jpg'%(frame))
+          # fn = os.path.join('mot_benchmark', phase, seq, 'img1', '%06d.jpg'%(frame))
+          fn = os.path.join('mot_benchmark', phase, seq, seq + '_000%09d_rendered.png'%(frame))
           im =io.imread(fn)
           ax1.imshow(im)
-          plt.title(seq + ' Tracked Targets')
+          plt.title(seq + ' Tracked Targets, frame: %d'%(frame))
 
         start_time = time.time()
         trackers = mot_tracker.update(dets)
